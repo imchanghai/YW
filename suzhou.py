cf2a4c43c33456b8a8f59616c2094cc4120a22f6
imchanghai <102135138+imchanghai@users.noreply.github.com>
2026-03-21 22:20:46 -0700

    Add files via upload

---
+++ suzhou.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from xml.dom import minidom
+import os 
+import datetime
+import subprocess
+
+
+
+#write XML code to file system
+xml_str = "this is test1"
+
+save_path_file = "suzhoutest.xml"
+
+with open(save_path_file, "wb") as f:
+    f.write(xml_str) 
+    
